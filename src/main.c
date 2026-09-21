d23946e9d9635db4b38d9919f7c1d5e8ff274a14
Emmery <emmerykelseymendoza@gmail.com>
2026-09-22 00:46:18 +0800

    Implement FreeRTOS queue for sensor communication

--- src/main.c
+++ src/main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "esp_adc/adc_oneshot.h"
 
 adc_oneshot_unit_handle_t adc_handle;
 
 void ldrTask(void *pv)
 {
-    int light = 0;
+    int light;
 
     while (1)
     {
-        adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &light); // GPIO34
+        adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &light);
         printf("[LDR] %d\n", light);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
@@ -22,7 +21,8 @@ void dhtTask(void *pv)
 {
     while (1)
     {
-        printf("[DHT22] Waiting for sensor...\n");
+        // Placeholder until the queue stage
+        printf("[DHT22] Temp: 24.0 C  Humidity: 40.0 %%\n");
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
@@ -31,17 +31,17 @@ void app_main(void)
 {
     printf("Multi-Sensor System Started\n");
 
-    adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT_1,
+    adc_oneshot_unit_init_cfg_t init_cfg = {
+        .unit_id = ADC_UNIT_1
     };
-    adc_oneshot_new_unit(&init_config, &adc_handle);
+    adc_oneshot_new_unit(&init_cfg, &adc_handle);
 
-    adc_oneshot_chan_cfg_t config = {
+    adc_oneshot_chan_cfg_t chan_cfg = {
         .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = ADC_ATTEN_DB_12,
+        .atten = ADC_ATTEN_DB_12
     };
-    adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &config);
+    adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &chan_cfg);
 
-    xTaskCreate(ldrTask, "LDR", 2048, NULL, 1, NULL);
-    xTaskCreate(dhtTask, "DHT", 2048, NULL, 1, NULL);
+    xTaskCreate(ldrTask, "LDR", 4096, NULL, 1, NULL);
+    xTaskCreate(dhtTask, "DHT", 4096, NULL, 1, NULL);
 }
