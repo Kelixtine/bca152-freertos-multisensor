d404fed309e1e8a057597849fa8ceb06e7d5043e
Emmery <emmerykelseymendoza@gmail.com>
2026-09-23 05:18:00 +0800

    Add temperature alarm test for exact lower threshold

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -18,11 +18,20 @@ void test_temp_below_lower(void)
     );
 }
 
+void test_temp_exactly_lower(void)
+{
+    TEST_ASSERT_EQUAL(
+        ALARM_LOW_TEMPERATURE,
+        evaluateTemperature(18.0f)
+    );
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_temp_below_lower);
+    RUN_TEST(test_temp_exactly_lower);
 
     return UNITY_END();
 }
