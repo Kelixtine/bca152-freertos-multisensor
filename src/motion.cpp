200f257965adcb10c6f09f08163b31a913800bf4
Emmery <emmerykelseymendoza@gmail.com>
2026-09-22 23:23:08 +0800

    Progress: modular C++ FreeRTOS multisensor

--- src/motion.cpp
+++ src/motion.cpp
@@ -5,29 +5,31 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-
 #include "driver/gpio.h"
 
 #define PIR_SENSOR_PIN GPIO_NUM_33
 
 void vMotionTask(void *pvParameters)
 {
-    gpio_config_t pir = {};
-    pir.pin_bit_mask = (1ULL << PIR_SENSOR_PIN);
-    pir.mode = GPIO_MODE_INPUT;
-    pir.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    gpio_config(&pir);
+    gpio_config_t pir_conf = {};
+    pir_conf.pin_bit_mask = (1ULL << PIR_SENSOR_PIN);
+    pir_conf.mode = GPIO_MODE_INPUT;
+    pir_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    pir_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    pir_conf.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&pir_conf);
 
-    TickType_t lastActivity = xTaskGetTickCount();
-    const TickType_t timeout = pdMS_TO_TICKS(15000);
+    TickType_t lastMotionTime = xTaskGetTickCount();
+    const TickType_t inactivityTimeout = pdMS_TO_TICKS(15000);
 
     while (1)
     {
-        EventBits_t bits = xEventGroupGetBits(g_systemEvents);
+        int motionState = gpio_get_level(PIR_SENSOR_PIN);
 
-        if (gpio_get_level(PIR_SENSOR_PIN))
+        if (motionState)
         {
-            lastActivity = xTaskGetTickCount();
+            lastMotionTime = xTaskGetTickCount();
+
             g_systemState = SystemState::ACTIVE;
 
             xEventGroupSetBits(g_systemEvents,
@@ -35,14 +37,12 @@ void vMotionTask(void *pvParameters)
         }
         else
         {
-            xEventGroupClearBits(g_systemEvents, EVENT_MOTION);
+            xEventGroupClearBits(g_systemEvents,
+                                 EVENT_MOTION);
         }
 
-        // Any user interaction refreshes timeout
-        if (bits & EVENT_ACTIVE)
-            lastActivity = xTaskGetTickCount();
-
-        if ((xTaskGetTickCount() - lastActivity) >= timeout)
+        if (g_systemState == SystemState::ACTIVE &&
+            (xTaskGetTickCount() - lastMotionTime) >= inactivityTimeout)
         {
             g_systemState = SystemState::INACTIVE;
 
