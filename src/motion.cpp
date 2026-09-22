b3e825b2a92cd63365e09c741a91faab5dbde4b1
Emmery <emmerykelseymendoza@gmail.com>
2026-09-23 02:05:32 +0800

    Restore OLED sleep and display layout

--- src/motion.cpp
+++ src/motion.cpp
@@ -1,43 +1,44 @@
 #include "motion.h"
 #include "rtos_objects.h"
-#include "display.h"
+#include "sensors.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "driver/gpio.h"
 
-#define PIR_PIN GPIO_NUM_33
 #define INACTIVITY_MS 15000
 
-static TickType_t lastActivityTick = 0;
+static TickType_t lastMotionTick = 0;
 
 void motion_task(void *pvParameters)
 {
-    gpio_config_t io = {};
-    io.pin_bit_mask = (1ULL << PIR_PIN);
-    io.mode = GPIO_MODE_INPUT;
-    io.pull_down_en = GPIO_PULLDOWN_ENABLE;
-    gpio_config(&io);
+    gpio_config_t io_conf = {};
+    io_conf.pin_bit_mask = (1ULL << PIR_PIN);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    gpio_config(&io_conf);
 
-    lastActivityTick = xTaskGetTickCount();
+    lastMotionTick = xTaskGetTickCount();
     xEventGroupSetBits(systemEvents, EVENT_ACTIVE);
 
-    for (;;)
+    while (true)
     {
         bool motion = gpio_get_level(PIR_PIN);
 
         if (motion)
         {
-            lastActivityTick = xTaskGetTickCount();
+            lastMotionTick = xTaskGetTickCount();
             xEventGroupSetBits(systemEvents, EVENT_ACTIVE | EVENT_MOTION);
         }
         else
         {
             xEventGroupClearBits(systemEvents, EVENT_MOTION);
 
-            if ((xTaskGetTickCount() - lastActivityTick) >=
-                pdMS_TO_TICKS(INACTIVITY_MS))
+            TickType_t elapsed =
+                xTaskGetTickCount() - lastMotionTick;
+
+            if (elapsed >= pdMS_TO_TICKS(INACTIVITY_MS))
             {
                 xEventGroupClearBits(systemEvents, EVENT_ACTIVE);
             }
